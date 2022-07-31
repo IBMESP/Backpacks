96c4e0c9dc02f17635aa9c2db475ebd848e7de51
IBM_ESP <53451213+IBMESP@users.noreply.github.com>
2022-07-31 11:57:41 +0200

    Update Backpacks.java

--- src/main/java/com/gmail/ibmesp1/bp/Backpacks.java
+++ src/main/java/com/gmail/ibmesp1/bp/Backpacks.java
@@ -86,6 +86,7 @@ public final class Backpacks extends JavaPlugin {
         checkers.checkMaxBP();
         checkers.checkSize();
         rowsBP = checkers.getRowsBP();
+        maxBP = checkers.getMaxBP();
 
         Bukkit.getConsoleSender().sendMessage("[Backpacks] - Version: " + version + " Enabled - By Ib");
 
